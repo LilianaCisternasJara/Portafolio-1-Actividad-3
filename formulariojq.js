2fffa98689621f6292631ee7ac8a88616b578c93
LilianaCisternasJara <93949192+LilianaCisternasJara@users.noreply.github.com>
2022-02-15 14:09:03 -0300

    Add files via upload

---
+++ formulariojq.js
@@ -0,0 +1,9 @@
+$("#bEnviar").click(function () {
+
+    let nombres = $("#name").val();
+    let papellido = $("#firstName").val();
+    let sapellido = $("#secondName").val();
+    let edad = $("#age").val();
+    let domicilio = $("#address").val();
+    
+})
